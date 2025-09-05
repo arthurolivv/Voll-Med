ff83dd7dae29bb3337dbb3c971463596fda9bc26
arthurolivv <art.jacintho13@gmail.com>
2025-09-05 17:27:55 -0300

    Tratamento de erros 404 e 400

---
+++ api/src/main/java/med/voli/api/infra/TradadorDeErros.java
@@ -0,0 +1,33 @@
+package med.voli.api.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.rmi.NotBoundException;
+
+@RestControllerAdvice
+public class TradadorDeErros {
+
+    //Essa classe altera o padrao do retorno de Erro do Spring boot (erro 500) para erro 404 not found
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarErro404(){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException exceptionRecebida){
+        var erros = exceptionRecebida.getFieldErrors(); //pega e armazena a lista de erros que aconteceram na exception
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacaoDto::new).toList());
+    }
+
+    private record DadosErroValidacaoDto(String campo, String mensagem){
+
+        public DadosErroValidacaoDto(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
+    }
+}
